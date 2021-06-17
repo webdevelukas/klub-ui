4be58dea5c2a6c6d8f33beca6e8b14c0b96e303e
Lukas Kreidenweis <lukas@deinklub.de>
2021-06-17 17:07:44 +0200

    add page sections story

--- src/components/Sport/Standings.stories.tsx
+++ src/components/Sport/Standings.stories.tsx
@@ -1,6 +1,7 @@
 import { Story, Meta } from "@storybook/react";
 import Standings, { Props } from "./Standings";
 import { standings } from "../../api/standings";
+import PageSection from "../../elements/PageSection";
 
 export default {
   title: "Sport/Standings",
@@ -11,6 +12,14 @@ export default {
 } as Meta;
 
 const Template: Story<Props> = (args) => <Standings {...args} />;
+const PageTemplate: Story<Props> = (args) => (
+  <PageSection title="Tabelle">
+    <Standings {...args} />
+  </PageSection>
+);
 
 export const Soccer = Template.bind({});
 Soccer.args = { standings };
+
+export const StandingsPageSection = PageTemplate.bind({});
+StandingsPageSection.args = { standings };
